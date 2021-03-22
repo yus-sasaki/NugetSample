e3f2b5c83479ef6e9204e42d9e97a858dfde6a68
sasaki_y <yusuke.sasaki.j7z@jpgr.denso.com>
2021-03-22 14:19:57 +0900

    YML更新しTestを追加

---
+++ src/SlackNotification.Tests/SlackNotificationTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace SlackNotification.Tests
+{
+    [TestClass]
+    public class SlackNotificationTests
+    {
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            var slackNotificationService = new SlackNotificationService();
+            var result = await slackNotificationService.Notify(
+	            "Test",
+	            @"https://hooks.slack.com/services/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",
+	            "sasaki_y",
+	            "sasaki_y");
+        }
+    }
+}
